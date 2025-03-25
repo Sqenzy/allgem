69c38a18d5c1ecc6bbca0ba10cf25fa417081c0f
Sqenzy <sqenzy.36@gmail.com>
2025-03-25 18:19:52 +0100

    changing signup/login behavior

--- Allgem01/obj/Debug/net9.0/Allgem01.AssemblyInfo.cs
+++ Allgem01/obj/Debug/net9.0/Allgem01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Allgem01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+130a52a094f3bbdc0088db543372e7101b142021")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a2832aba5c5a0af472de78824b63d4dcb0ea3ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Allgem01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Allgem01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
