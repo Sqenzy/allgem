147646a62f5dcb84e0c630f05c59fa83c7d3302f
Sqenzy <sqenzy.36@gmail.com>
2025-04-04 20:35:36 +0200

    adding send message

--- Allgem01/obj/Debug/net9.0/Allgem01.AssemblyInfo.cs
+++ Allgem01/obj/Debug/net9.0/Allgem01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Allgem01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d9f181a36da1324ac10e7ef9e45e312cee68854")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aae479aa14c07dbe8110a80b2ddd9db18dae537")]
 [assembly: System.Reflection.AssemblyProductAttribute("Allgem01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Allgem01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
