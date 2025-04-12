46a4f5978845a8cc1cff8508edc5d1bb1d02b933
Sqenzy <sqenzy.36@gmail.com>
2025-04-12 16:35:44 +0200

    a

--- Allgem01/obj/Debug/net9.0/Allgem01.AssemblyInfo.cs
+++ Allgem01/obj/Debug/net9.0/Allgem01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Allgem01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8d7d4bd6209c3dbdff598a5e34f75272a063edd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbaae6338f0e00c3246a41aeb95105e8beab5370")]
 [assembly: System.Reflection.AssemblyProductAttribute("Allgem01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Allgem01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
