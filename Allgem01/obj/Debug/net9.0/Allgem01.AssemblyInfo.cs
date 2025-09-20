b3545b9c8b8f29b3782a6a5437cdc9c68d985fa0
Sqenzy <sqenzy.36@gmail.com>
2025-09-20 23:48:44 +0200

    allgem 2.0 header

--- Allgem01/obj/Debug/net9.0/Allgem01.AssemblyInfo.cs
+++ Allgem01/obj/Debug/net9.0/Allgem01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Allgem01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f3f2f02ecd170e675a699f91718cd7cdbfa15fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee18d049fd4e3bc5cb80c99ea5acbc2ef8d24e67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Allgem01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Allgem01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
